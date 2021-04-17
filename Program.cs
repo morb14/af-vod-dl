5bb5b983ce242e245854042c052ffb9758384374
morb14 <morb14@protonmail.com>
2021-04-17 07:57:24 -0400

    cool

--- Program.cs
+++ Program.cs
@@ -16,19 +16,19 @@ namespace Program
             date = Console.ReadLine();
             if(isWindows)
             {
-                Console.WriteLine("ok i download " + date + " VOD now :D\n");
+                Console.WriteLine("ok i download " + date + " VOD now :D");
                 System.Diagnostics.Process process = new System.Diagnostics.Process();
 			    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
 			    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
 			    startInfo.FileName = "yt-dlp.exe";
-			    startInfo.Arguments = "-i --prefer-ffmpeg --merge-output-format mp4 --add-metadata --write-thumbnail --verbose --force-ipv4 --ignore-errors --no-continue --no-overwrites --embed-thumbnail -o " + date + " --no-check-certificate --force-generic-extractor https://cdn2.foxtrotstream.xyz/vod_local/af/" + date + "/index.m3u8";
+			    startInfo.Arguments = "-i --prefer-ffmpeg --merge-output-format mp4 --add-metadata --write-thumbnail --verbose --force-ipv4 --ignore-errors --no-continue --no-overwrites --embed-thumbnail -o " + date + ".mp4 --no-check-certificate --force-generic-extractor https://cdn2.foxtrotstream.xyz/vod_local/af/" + date + "/index.m3u8";
 			    process.StartInfo = startInfo;
 			    process.Start();
             }
             else
             {
                 Console.WriteLine("copy and paste this line into your terminal vvv (must have yt-dlp and ffmpeg installed)");
-                Console.WriteLine("yt-dlp -i --prefer-ffmpeg --merge-output-format mp4 --add-metadata --write-thumbnail --verbose --force-ipv4 --ignore-errors --no-continue --no-overwrites --embed-thumbnail --no-check-certificate --force-generic-extractor https://cdn2.foxtrotstream.xyz/vod_local/af/" + date + "/index.m3u8");
+                Console.WriteLine("yt-dlp -i --prefer-ffmpeg --merge-output-format mp4 --add-metadata --write-thumbnail --verbose --force-ipv4 --ignore-errors --no-continue --no-overwrites --embed-thumbnail -o " + date + ".mp4 --no-check-certificate --force-generic-extractor https://cdn2.foxtrotstream.xyz/vod_local/af/" + date + "/index.m3u8";
             }
 			Console.ReadLine();
 			Environment.Exit(0);
